c3c745dc482cedf5d777f4a6bfea3813e59c6b34
iMeisa <iMeisa@github.com>
2021-11-17 14:03:25 +0200

    Add HTML

--- main.go
+++ main.go
@@ -7,19 +7,6 @@ import (
 
 const portNumber = ":8080"
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	_, _ =fmt.Fprintf(w, "This is the home page")
-}
-
-func About(w http.ResponseWriter, r *http.Request) {
-	sum := addValues(2, 2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
-}
-
-func addValues(x, y int) int {
-	return x + y
-}
-
 func main() {
 
 	http.HandleFunc("/", Home)
