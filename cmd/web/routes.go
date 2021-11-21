1433d1709f11cab2dd5de5429b29a8e346b1f0b3
iMeisa <62470487+iMeisa@users.noreply.github.com>
2021-11-21 22:30:16 +0200

    Routers

---
+++ cmd/web/routes.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"meisa_xyz/pkg/config"
+	"meisa_xyz/pkg/handlers"
+	"net/http"
+)
+
+func routes(_ *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module meisa_xyz
 
 go 1.16
+
+require github.com/go-chi/chi v1.5.4
