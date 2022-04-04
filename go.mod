24e32df12b33b65225571048110cf1d928c81fb0
iMeisa <62470487+iMeisa@users.noreply.github.com>
2022-04-04 12:22:35 +0300

    Created db driver

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/alexedwards/scs/v2 v2.4.0 // indirect
 	github.com/go-chi/chi v1.5.4
 	github.com/justinas/nosurf v1.1.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.9
 )
---
+++ internal/driver/driver.go
@@ -0,0 +1,56 @@
+package driver
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"time"
+)
+
+// DB holds the database connection pool
+type DB struct {
+	SQL *sql.DB
+}
+
+var dbConn = &DB{}
+
+const maxOpenDbConn = 10
+const maxIdleDbConn = 5
+const connMaxLifetime = 5 * time.Minute
+
+// ConnectSQL creates db pool for sqlite
+func ConnectSQL(dsn string) (*DB, error) {
+	db, err := NewDatabase(dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetMaxOpenConns(maxOpenDbConn)
+	db.SetMaxIdleConns(maxIdleDbConn)
+	db.SetConnMaxLifetime(connMaxLifetime)
+
+	dbConn.SQL = db
+
+	return dbConn, nil
+}
+
+// testDB tries to ping the database
+func testDB(db *sql.DB) error {
+	if err := db.Ping(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// NewDatabase creates a new database for the application
+func NewDatabase(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
