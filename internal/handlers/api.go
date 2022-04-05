64b794be1ed39bb5b2e447a4c0d09060a90aaad5
iMeisa <62470487+iMeisa@users.noreply.github.com>
2022-04-05 10:15:24 +0300

    Created api handlers file

---
+++ internal/handlers/api.go
@@ -0,0 +1,41 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func (m *Repository) GetAllDrivers(w http.ResponseWriter, r *http.Request) {
+	m.DB.CreateDriverTable()
+
+	drivers, err := m.DB.QueryAllDrivers()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	driversJSON, err := json.Marshal(drivers)
+	if err != nil {
+		log.Fatal("Could not convert to JSON:", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, _ = w.Write(driversJSON)
+}
+
+func (m *Repository) GetAllTeams(w http.ResponseWriter, r *http.Request) {
+	m.DB.CreateTeamTable()
+
+	teams, err := m.DB.QueryAllTeams()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	teamsJSON, err := json.Marshal(teams)
+	if err != nil {
+		log.Fatal("Could not convert to JSON:", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, _ = w.Write(teamsJSON)
+}
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -48,40 +48,6 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
-func (m *Repository) GetAllDrivers(w http.ResponseWriter, r *http.Request) {
-	m.DB.CreateDriverTable()
-
-	drivers, err := m.DB.QueryAllDrivers()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	driversJSON, err := json.Marshal(drivers)
-	if err != nil {
-		log.Fatal("Could not convert to JSON:", err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	_, _ = w.Write(driversJSON)
-}
-
-func (m *Repository) GetAllTeams(w http.ResponseWriter, r *http.Request) {
-	m.DB.CreateTeamTable()
-
-	teams, err := m.DB.QueryAllTeams()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	teamsJSON, err := json.Marshal(teams)
-	if err != nil {
-		log.Fatal("Could not convert to JSON:", err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	_, _ = w.Write(teamsJSON)
-}
-
 func (m *Repository) Edit(w http.ResponseWriter, r *http.Request) {
 	render.Template(w, "edit.page.tmpl", &models.TemplateData{})
 }
