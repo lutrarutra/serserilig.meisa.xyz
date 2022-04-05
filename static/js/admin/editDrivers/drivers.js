944309cdd7d7e42d188078d88b69e1820a431132
iMeisa <62470487+iMeisa@users.noreply.github.com>
2022-04-05 12:22:48 +0300

    You can now add drivers with the edit page

--- static/js/admin/editDrivers/drivers.js
+++ static/js/admin/editDrivers/drivers.js
@@ -1,3 +1,23 @@
+function addDriver() {
+    const name_input = document.getElementById('add-driver-name')
+    const valid_input = validateInput(name_input)
+
+    const success_alert = document.getElementById('add-success')
+    if (valid_input) {
+        success_alert.removeAttribute('hidden')
+
+        const add_url = '/api/drivers/add?name=' + name_input.value
+        fetch(add_url)
+            .then((response) => {
+            return response.json()
+            }).then((data) => {
+                console.log(data)
+            })
+        
+        setTimeout(() => {location.reload()}, 2000)
+    }
+}
+
 function buildDriverTable() {
     const driver_list = document.getElementById('driver-list')
     const url = '/api/drivers'
@@ -18,4 +38,18 @@ function buildDriverTable() {
                 `
             }
         })
+}
+
+function validateInput(input_element) {
+    if (input_element.value.length < 1) {
+        console.log('invalid')
+        input_element.classList.remove('border-success')
+        input_element.classList.add('border-danger')
+        return false
+    }
+
+    console.log('valid')
+    input_element.classList.remove('border-danger')
+    input_element.classList.add('border-success')
+    return true
 }
