1f4410dd92662a963190317815bd2c1855831784
lutrarutra <artur.gynter@hotmail.com>
2022-04-16 23:43:02 +0300

    First iteration of calendar

--- internal/handlers/admin.go
+++ internal/handlers/admin.go
@@ -1,8 +1,9 @@
 package handlers
 
 import (
-	"github.com/iMeisa/serserilig.meisa.xyz/internal/render"
 	"net/http"
+
+	"github.com/iMeisa/serserilig.meisa.xyz/internal/render"
 )
 
 func (m *Repository) EditDrivers(w http.ResponseWriter, r *http.Request) {
@@ -15,22 +16,14 @@ func (m *Repository) EditTeams(w http.ResponseWriter, r *http.Request) {
 	templateData.GetDrivers()
 	templateData.GetTeams()
 
-	driverNames := make(map[int]string)
-	var reserveDrivers []string
-	for _, driver := range templateData.Drivers {
-		if driver.TeamID == -1 {
-			reserveDrivers = append(reserveDrivers, driver.Name)
-		}
-		driverNames[driver.ID] = driver.Name
-	}
-
-	dataMap := make(map[string]interface{})
-
-	dataMap["driver_names"] = driverNames
-	dataMap["reserve_drivers"] = reserveDrivers
-	templateData.Data = dataMap
-
 	render.Template(w, r, "editteams.page.tmpl", templateData)
+}
+
+func (m *Repository) EditCalendar(w http.ResponseWriter, r *http.Request) {
+	templateData.GetRaces()
+	templateData.GetGrandPrixes()
+
+	render.Template(w, r, "editcalendar.page.tmpl", templateData)
 }
 
 func (m *Repository) Edit(w http.ResponseWriter, r *http.Request) {
--- internal/models/templatedata.go
+++ internal/models/templatedata.go
@@ -4,23 +4,64 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"log"
+	"sort"
 )
 
 // TemplateData holds data sent from handlers to templates
 type TemplateData struct {
-	StringMap  map[string]string
-	IntMap     map[string]int
-	FloatMap   map[string]float32
-	Data       map[string]interface{}
-	Drivers    []Driver
-	Teams      []Team
-	CSRFToken  string
-	Flash      string // Some message
-	Warning    string
-	Error      string
+	StringMap map[string]string
+	IntMap    map[string]int
+	FloatMap  map[string]float32
+	Data      map[string]interface{}
+	Drivers   []Driver
+	Teams     []Team
+	Races     []Race
+	GPs       []GrandPrix
+	CSRFToken string
+	Flash     string // Some message
+	Warning   string
+	Error     string
 }
 
-func(data *TemplateData) GetDrivers() {
+func (data *TemplateData) GetRaces() {
+	var races []Race
+	rawFile, err := ioutil.ReadFile("./static/json/calendar.json")
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = json.Unmarshal(rawFile, &races)
+	if err != nil {
+		log.Println(err)
+	}
+
+	sort.Slice(races, func(i, j int) bool {
+		return races[i].Date < races[j].Date
+	})
+
+	data.Races = races
+}
+
+func (data *TemplateData) GetGrandPrixes() {
+	var gps []GrandPrix
+	rawFile, err := ioutil.ReadFile("./static/json/gp_reference.json")
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = json.Unmarshal(rawFile, &gps)
+	if err != nil {
+		log.Println(err)
+	}
+
+	sort.Slice(gps, func(i, j int) bool {
+		return gps[i].Country < gps[j].Country
+	})
+
+	data.GPs = gps
+}
+
+func (data *TemplateData) GetDrivers() {
 	var drivers []Driver
 	rawFile, err := ioutil.ReadFile("./static/json/drivers.json")
 	if err != nil {
@@ -32,16 +73,16 @@ func(data *TemplateData) GetDrivers() {
 	}
 
 	noDriver := Driver{
-		ID: -1,
-		Name: "No Driver",
+		ID:            -1,
+		Name:          "No Driver",
 		PenaltyPoints: 0,
 	}
 	drivers = append(drivers, noDriver)
-	
+
 	data.Drivers = drivers
 }
 
-func(data *TemplateData) GetTeams() {
+func (data *TemplateData) GetTeams() {
 	var teams []Team
 	rawFile, err := ioutil.ReadFile("./static/json/teams.json")
 	if err != nil {
